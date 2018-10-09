8db6a74cdca5a82be2609b894423dd882d2a7311
Erfan Ebrahimi <info.yeganeh@samenblog.com>
2018-10-09 12:42:46 +0330

    Add files via upload

---
+++ edd-payping.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: افزونه پرداخت پی‌پینگ برای Easy Digital Downloads
+Version: 1.0
+Description:  افزونه درگاه پرداخت پی‌پینگ برای Easy Digital Downloads
+Plugin URI: https://www.payping.ir/
+Author: Erfan Ebrahimi
+Author URI: http://erfanebrahimi.ir/
+
+*/
+
+// Toman Currency
+require 'includes/toman-currency.php';
+
+// Include the main file
+require 'gateways/payping.php';
